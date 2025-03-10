7daee35731911f6d7f948c96d207f35132d5c7c7
hahich <kurorumi0x@gmail.com>
2025-03-10 22:54:58 +0700

    index.js 2

--- be/index.js
+++ be/index.js
@@ -11,18 +11,16 @@ import categoryRouter from "./routes/category.route.js";
 import uploadRouter from "./routes/upload.route.js";
 import subCategoryRouter from "./routes/subCategory.route.js";
 import productRouter from "./routes/products.route.js";
-import cartRouter from "./routes/cart.route.js"
+import cartRouter from "./routes/cart.route.js";
 import addressRouter from "./routes/address.route.js";
 import orderRouter from "./routes/order.route.js";
-
 const app = express();
-
+const port = process.env.PORT || 8080;
 // Cấu hình CORS với nguồn cụ thể
 const corsOptions = {
   origin: 'http://localhost:5173', // Đặt nguồn mà bạn muốn cho phép
   credentials: true, // Cho phép gửi cookie
 };
-
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
@@ -32,28 +30,26 @@ app.use(
     crossOriginResourcePolicy: false,
   })
 );
-
-const port = process.env.PORT || 8080;
-
 app.get("/", (req, res) => {
   res.json({
     message: "Server is running on port " + port,
   });
 });
-
+// Đăng ký các route
 app.use("/api/user", userRouter);
-app.use("/api/category", categoryRouter)
-app.use("/api/file", uploadRouter)
-app.use("/api/subCategory", subCategoryRouter)
-app.use("/api/products", productRouter)
-app.use("/api/cart", cartRouter)
-app.use("/api/address", addressRouter)
-app.use("/api/order", orderRouter)
-
+app.use("/api/category", categoryRouter);
+app.use("/api/file", uploadRouter);
+app.use("/api/subCategory", subCategoryRouter);
+app.use("/api/products", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/address", addressRouter);
+app.use("/api/order", orderRouter);
+// Kết nối đến cơ sở dữ liệu và khởi động server
+let server; // Định nghĩa biến server ở mức toàn cục
 connectDB().then(() => {
-  const server = app.listen(port, () => {
+  server = app.listen(port, () => {
     console.log("Server is running on port", port);
   });
 });
-
-export default server
+// Xuất server
+export default server;
